f59133b36b66c746b0c4af86efac76eebdbb38d0
oliverlangmo <oliver.langmo@gmail.com>
2017-04-25 11:31:15 -0500

    added js to check for some and every, also find and findIndex

--- scripts.js
+++ scripts.js
@@ -17,12 +17,33 @@
 
     // Some and Every Checks
     // Array.prototype.some() // is at least one person 19 or older?
+    const isAdult = people.some(person => {
+      const currentYear = (new Date()).getFullYear();
+      return currentYear - person.year >= 19;
+    });
+    console.log(isAdult);
     // Array.prototype.every() // is everyone 19 or older?
-
+    const allAdults = people.every(person => {
+      const currentYear = (new Date()).getFullYear();
+      return currentYear - person.year >= 19;
+    });
+    console.log(allAdults);
     // Array.prototype.find()
     // Find is like filter, but instead returns just the one you are looking for
     // find the comment with the ID of 823423
-
+    const comment = comments.find(comment => comment.id === 823423);
+     console.log(comment);
     // Array.prototype.findIndex()
+    const index = comments.findIndex(comment => comment.id === 823423);
+    console.log(index);
     // Find the comment with this ID
     // delete the comment with the ID of 823423
+    
+    //comments.splice(index,1); < this edits current comments array
+
+  //but this one creates a new array without the ID 823423 comment
+    const newComments = [
+      ...comments.slice(0, index),
+      ...comments.slice(index + 1)
+    ];
+console.table(newComments);
